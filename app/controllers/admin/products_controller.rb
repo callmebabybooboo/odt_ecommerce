e9e8fa7f03bbea25b497f47e4a096d8f525bacfe
callmebaby <napawan.lua@gmail.com>
2025-04-22 02:35:18 +0700

    Product: Create a product form with required fields.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,22 @@
 class Admin::ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_products_path, notice: "สร้างสินค้าเรียบร้อยแล้ว"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name, :description, :category, :base_price, :status, :stock, :cover_image)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   has_many :variants, dependent: :destroy
+  has_one_attached :cover_image
+
+  validates :name, :description, :category, :base_price, :status, :stock, presence: true
 end
