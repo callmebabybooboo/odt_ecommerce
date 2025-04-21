5a33e8a1f18f95a9e708f5395e40712c5396873a
callmebaby <napawan.lua@gmail.com>
2025-04-22 02:55:19 +0700

    Product: Update category field to use select dropdown with predefined options

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,7 @@ class Product < ApplicationRecord
   has_one_attached :cover_image
 
   validates :name, :description, :category, :base_price, :status, :stock, presence: true
+
+  CATEGORIES = ["เสื้อผ้า", "แก้ว", "หมวก", "ของใช้ทั่วไป", "อื่นๆ"]
+  validates :category, inclusion: { in: CATEGORIES, message: "กรุณาเลือกหมวดหมู่" }
 end
