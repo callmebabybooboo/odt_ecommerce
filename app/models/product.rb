d9c3df36f565e277a374f57b9f837d2e71e571c5
callmebaby <napawan.lua@gmail.com>
2025-04-22 03:40:03 +0700

    Product: Refactor product form to use predefined status options

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,9 @@ class Product < ApplicationRecord
 
   validates :name, :description, :category, :base_price, :status, :stock, presence: true
 
-  CATEGORIES = ["เสื้อผ้า", "แก้ว", "หมวก", "ของใช้ทั่วไป", "อื่นๆ"]
+  CATEGORIES = [ "เสื้อผ้า", "แก้ว", "หมวก", "ของใช้ทั่วไป", "อื่นๆ" ]
   validates :category, inclusion: { in: CATEGORIES, message: "กรุณาเลือกหมวดหมู่" }
+
+  STATUS_OPTIONS = [ "แสดงสินค้า", "ไม่แสดงสินค้า" ]
+  validates :status, inclusion: { in: STATUS_OPTIONS, message: "กรุณาเลือกสถานะ" }
 end
