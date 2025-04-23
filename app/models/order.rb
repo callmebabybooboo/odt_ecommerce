5c171e661b7f5f31d67c4cdb2de910f76e3ed552
callmebaby <napawan.lua@gmail.com>
2025-04-24 03:16:46 +0700

    Order: Enhance Order and OrderItem models with validations; update User model to establish order associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :order_items, dependent: :destroy
+
+  enum status: { pending: "pending", paid: "paid" }
+
+  validates :total_price, numericality: { greater_than_or_equal_to: 0 }
 end
