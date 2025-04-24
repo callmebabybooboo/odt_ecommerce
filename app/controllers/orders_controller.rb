01ecfa505abfa2837e7be396233818dbf55ad2c0
callmebaby <napawan.lua@gmail.com>
2025-04-24 12:29:49 +0700

    Order: Implement order index view and update routes to include index action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @orders = current_user.orders.order(created_at: :desc)
+  end
+
   def new
     @order = Order.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :cart, only: [ :show ]
   resources :cart_items, only: [ :create, :update, :destroy ]
-  resources :orders, only: [ :new, :create, :show ] do
+  resources :orders, only: [ :index, :new, :create, :show ] do
     member do
       patch :pay
     end
